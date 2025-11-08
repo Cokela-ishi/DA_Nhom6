5337999dca80bcfcfb568e69d83a73962a7b6af4
anhhoang <hoangngouen5452@gmail.com>
2025-11-09 02:15:20 +0700

    cập nhật bảng băm, class hàng hoá, windows forms

---
+++ DoAnTinHoc/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnTinHoc
+{
+    public partial class frmQLKH : Form
+    {
+        public frmQLKH()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvQLKH_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Bạn có chắc chắn muốn thoát khỏi chương trình",
+               "Xác nhận thoát",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if(result == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+    }
+}
